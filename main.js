49f90ed937bf1d23e04bb23aa31685b67ae28794
IshanBhati <119059307+IshanBhati@users.noreply.github.com>
2023-06-13 20:28:14 +0530

    Update main.js

--- main.js
+++ main.js
@@ -19,15 +19,15 @@ function draw()
 {
     image(img, 0, 0, 450, 450);
     fill("#FF0000");
-    stroke("#FF0000");
     text("Dog", 45, 75);
     noFill();
+    stroke("#FF0000");
     rect(30, 60, 450, 350);
 
     fill("#FF0000");
-    stroke("#FF0000");
     text("Cat", 320, 120);
     noFill();
+    stroke("#FF0000");
     rect(300, 90, 270, 320);
 }
 
